7d10e1f3dadef4c62797fdec6ccf714b3edae318
b.esocbari <b.escobari@bo.berlin>
2025-05-21 19:25:28 +0200

    connect DB

--- my_project/settings.py
+++ my_project/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": 'django.db.backends.mysql',
+        "NAME": 'DNA_Storage_Request',
+        "USER": 'admin',
+        "PASSWORD": '1234',
+        "HOST": 'localhost',
+        "PORT": 3306,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
